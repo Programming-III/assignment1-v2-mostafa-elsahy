c96b9b686a30bef67dab9b49491763f8eea7905a
mostafa-elsahy <moustaasraf@gmail.com>
2025-11-11 09:01:20 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -6,16 +6,13 @@
 #using namespace std;
 //#write student class here
 
-
-
-
-
-
-
-
-
-
-
-
+class Student : public Person{
+private:
+	int yearLevel;
+	string major;
+public:
+	Student(string n, int i, int y, string m);
+	Student();
+	void display();
 
 #endif
