55048a95c1c594c9281157dac25ab9797a9e9245
mostafa-elsahy <moustaasraf@gmail.com>
2025-11-11 09:45:26 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,40 +9,91 @@
 using namespace std;
 
 // ==================== Person Class Implementation =========================
+Person::Person(string n, int i)
+{
+	name = n;
+	id = i;
+}
 
+Person::Person(){}
 
+Person::display()
+{
+	cout << "Name: " << name << endl << "Id: " << id;
+}
 
+Person::getName(){
+	return name;
+}
 
-
-
-
+Person::~Person(){}
 
 // ==================== Student Class Implementation ====================
 
+Student::Student(string n, int i, int y, int m) : Person(n, i)
+{
+	yearLevel = y;
+	major = m;
+}
 
 
+Student::Student(){}
 
+Student::display()
+{
+	cout <<  "Student Info:" << endl << "Name: " << getName << endl << "Major " << major << "Year " << yearLevel;
+}
 
 
 // ==================== Instructor Class Implementation ====================
-
-
+Instructor::Instructor(string n, int i, string d, int e) : Person(n, i)
+{
+	department = d;
+	experienceYears = e;
+}
+Instructor:: Instructor(){}
+Instructor::display()
+{
+	cout << "Instructor Info: " << endl << "Name: Dr. " << getName << endl << "Department: " << department << endl << "Experience" << experienceYears;
+}
 
 
 
 
 // ==================== Course Class Implementation ====================
-
-
-
-
-
-
-
-
-// ==================== Main Function ====================
-int main() {
-   
-    
-    return 0;
+Course::Course(string code, string name, int max)
+{
+	courseCode  = code;
+	courseName = name;
+	maxStudents = max;
+	students = new Student[max];
+	currentStudents = 0;
 }
+Course::addStudent(const Student& s){
+	if (currentStudents != maxStudents)
+	{
+		students[currentStudents] == s;
+		currentStudents++;
+	}
+	else{
+		cout << "Course is Full" << endl;
+	}
+}
+Course :: displayCourseInfo()
+{
+	cout << "Course: " << courseCode << " " << courseName << endl << "Max Students" << maxStudents << "Currently Enrolled" << endl;
+	for(int i = 0; i < maxStudents; i++)
+	{
+		cout << students[i].getName() << student[i].getId();
+	}
+}
+
+
+
+
+int main() {
+	Student s ("Mostafa", 1600, 2, "CS");
+	Instructor I ("Mostafa", 121, "CS", 12);
+	Course C 
+}
+
