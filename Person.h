7740378d437330191f0e04a1c9074be86591ede9
mostafa-elsahy <moustaasraf@gmail.com>
2025-11-11 08:54:08 +0200

    Add Person class definition
    Define Person class with private attributes and public methods.

--- Person.h
+++ Person.h
@@ -4,17 +4,14 @@
 #include <string>
 using namespace std;
 
-//#write Person class here
-
-
-
-
-
-
-
-
-
-
-
+class Person{
+private:
+	string name;
+	int id;
+public:
+	Person(string n, int i);
+	Person();
+	void display();
+};
 
 #endif
